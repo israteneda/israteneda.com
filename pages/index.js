ec73000d9e3ff898c1ce6a302a5c148746f7df8d
Israel Teneda <israteneda@gmail.com>
2022-08-31 00:16:30 -0500

    fix: lint

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
-import Link from 'next/link';
-import Date from '../components/date';
+import Link from "next/link";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -22,19 +22,19 @@ export default function Home({ allPostsData }) {
       </Head>
       <section>
         <p>
-          I'm a software developer with 3 years of experience. I'm passionate
+          I&apos;m a software developer with 3 years of experience. I&apos;m passionate
           about learning and sharing knowledge with 1the engineering community
           through my personal blog and my YouTube channel. Furthermore, I have
           skills in a broad variety of technologies, methodologies, platforms
           and programming languages like Python, JavaScript, Docker, Git, TDD,
-          QA, among others. 
+          QA, among others.
         </p>
         <p>
-          Moreover, I like to contribute with the growth of
-          the software community, contributing to open source and answer
-          questions on Stack Overflow. Constantly I'm learning new agile
-          methodologies, good practices for coding or soft skills to improve
-          myself as a professional.
+          Moreover, I like to contribute with the growth of the software
+          community, contributing to open source and answer questions on Stack
+          Overflow. Constantly I&apos;m learning new agile methodologies, good
+          practices for coding or soft skills to improve myself as a
+          professional.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
