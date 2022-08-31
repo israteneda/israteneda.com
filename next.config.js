c918b5b2340ccaefd701bbeee5845dc2d776b322
Israel Teneda <israteneda@gmail.com>
2022-08-31 01:52:08 -0500

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /**
-   * @type {import('next').NextConfig}
-   */
- const nextConfig   = {
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   images: {
     loader: 'akamai',
     path: '',
@@ -9,4 +9,4 @@
   assetPrefix: './',
 };
 
-export default   nextConfig;
+module.exports = nextConfig;
