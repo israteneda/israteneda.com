9f4468d2cf3bba81c2f479d4680b706677f46669
Israel Teneda <israteneda@gmail.com>
2022-08-31 01:48:41 -0500

    add a github action

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  assetPrefix: './',
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
+   * @type {import('next').NextConfig}
+   */
+ const nextConfig   = {
+  images: {
+    loader: 'akamai',
+    path: '',
   },
+  assetPrefix: './',
 };
 
-module.exports = nextConfig;
+export default   nextConfig;
